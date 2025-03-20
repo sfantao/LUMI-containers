c2b3e09523097591bb3400cd61636b03c31411b5
Samuel Antao <samuelfantao@gmail.com>
2025-03-20 02:54:48 +0000

    Test container build.

--- RecipesDocker/helper-files/cpe-24.03.sh
+++ RecipesDocker/helper-files/cpe-24.03.sh
@@ -2,12 +2,18 @@
 
 set -o pipefail
 
-if [ ! -f cpe-24.03.tar ] ; then
+if [ -z ${PERSIST_CPE_FOLDER} ] ; then
+  PERSIST_CPE_FOLDER='.'
+fi
+
+if [ ! -f ${PERSIST_CPE_FOLDER}/cpe-24.03.tar ] ; then
 
   ssh lumi 'bash -c -xe "cd / ; tar -cf - \
     opt/cray/libfabric/1.15.2.0/ \
     opt/cray/pe/mpich/8.1.29/ofi/crayclang/17.0/ \
     opt/cray/pe/mpich/8.1.29/gtl/lib/libmpi_gtl_hsa.* \
-    usr/lib64/libcxi.so*"' | cat > cpe-24.03.tar
+    usr/lib64/libcxi.so*"' | cat > ${PERSIST_CPE_FOLDER}/cpe-24.03.tar
 
 fi
+
+cp -rf ${PERSIST_CPE_FOLDER}/cpe-24.03.tar .
